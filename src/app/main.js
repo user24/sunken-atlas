8c6b97ef931c4bc151f89a4d890790f938513876
Howard Yeend <howard.yeend@gmail.com>
2023-06-27 12:39:38 +0100

    edit function, tiles used warning

--- src/app/main.js
+++ src/app/main.js
@@ -1,7 +1,7 @@
 import classes from './main.module.css'
 import { useState, useEffect } from 'react';
 import Island from './Island';
-import { string2island } from '@/utils/islandParser/islandParser';
+import { string2island, island2String } from '@/utils/islandParser/islandParser';
 import islands from './data/islands.json';
 import { IM_Fell_English_SC as Font } from 'next/font/google';
 
@@ -15,11 +15,16 @@ const hostname = 'https://sunkenatlas.com';
 export default function Main({islandString='', loading}) {
 
   const [copyLabel, setCopyLabel] = useState('Copy');
-  const tiles = string2island(islandString);
-  const link = `${hostname}/i/${islandString}`;
+  const [editString, setEditString] = useState(islandString);
+  const tiles = string2island(editString);
+  
+  const getLink = () => {
+    return `https://sunkenatlas.com/i/${editString}`;
+  };
 
   useEffect(() => {
     setCopyLabel('Copy');
+    setEditString(islandString);
   }, [islandString]);
 
   const selectMe = (e) => {
@@ -31,17 +36,43 @@ export default function Main({islandString='', loading}) {
     setCopyLabel('Copied');
   }
 
+  const toggleTile = (x, y) => {
+    const newTiles = [...tiles];
+    newTiles[y][x] = newTiles[y][x] ? 0 : 1;
+    //newIsland.name = getIslandName(editingIsland.code);
+    setEditString(island2String(newTiles));
+    // window.history.pushState({}, '', '?i='+newIsland.code);
+  };
+
+  const getTileNumberWarning = () => {
+    const sumTiles = tiles.reduce((length, row) => length + row.filter(cell => cell === 1).length, 0);
+    const difference = 24 - sumTiles;
+    const plural = (Math.abs(difference) === 1) ? '' : 's';
+    if(difference > 0) {
+      return `${difference} tile${plural} still to place`;
+    } else if (difference < 0) {
+      let diff = Math.abs(difference);
+      if (diff === 1) {
+        diff = 'an';
+      }
+      return `You've placed ${diff} extra tile${plural}!`;
+    } else {
+      return '';
+    }
+  }
+
   return (
     <div className={classes.wrapper}>
       <main className={classes.main}>
         <h1 className={[font.className, classes.h1].join(' ')}>The Sunken Atlas</h1>
         <div className={classes.welcome}>Alternate layouts &amp; map editor for Forbidden Island</div>
-        <Island tiles={tiles} />
+        <Island editable toggleTile={toggleTile} tiles={tiles} />
+        <div className={classes.tileWarning}>{getTileNumberWarning()}</div>
         {loading && islandString ? null : (
           <>
             <h2 className={classes.h2}>Share this island</h2>
             <div className={classes.share}>
-              <input className={classes.shareBox} type='text' value={link} readOnly onClick={selectMe} />
+              <input className={classes.shareBox} type='text' value={getLink()} readOnly onClick={selectMe} />
               <button className={classes.shareButton} onClick={copy}>{copyLabel}</button>
             </div>
           </>
