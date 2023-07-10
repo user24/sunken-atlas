ac9e9cd3ac8f4fae299acdac4bb9471ed7d10537
Howard Yeend <howard.yeend@gmail.com>
2023-07-10 08:37:15 +0100

    wip: header with version, offset, rowLength, more tests

--- src/utils/islandParser/islandParser.js
+++ src/utils/islandParser/islandParser.js
@@ -5,7 +5,7 @@ if (ALPHABET.length < 64) {
   throw new Error('Alphabet not long enough to encode 6 bit characters (111111==64)');
 }
 
-const char2bin = (char) => ('000000' + ALPHABET.indexOf(char).toString(2)).slice(-6);
+const dec2bin = (dec, minLength = 6) => ('00000000000000000000' + (dec.toString(2))).slice(0 - minLength);
 const transpose = (matrix) => matrix[0].map((col, i) => matrix.map(row => row[i]));
 const flatten = (arr) => [].concat.apply([], arr);
 const allZeroes = (row) => row && (row.join('').replaceAll('0', '').length === 0);
@@ -36,36 +36,82 @@ const trimEmptyPadding = isle => {
 
 const isEven = x => x % 2 === 0;
 
+const decodeHeader = header => {
+  header = string2binary(header);
+  const [nada ,version, cols, offsetX, offsetY] = header.match(/^(.{2})(.{4})(.{3})(.{3})/).map(resultBin => parseInt(resultBin, 2) || 0);
+
+  return {
+    version,
+    cols,
+    offsetX,
+    offsetY
+  };
+};
+
+const string2binary = str => {
+  return str.split('').map(char => dec2bin(ALPHABET.indexOf(char))).join('');
+};
+const binary2string = bin => {
+  return (bin.match(/.{1,6}/g) || []).map(binStr => ALPHABET.charAt(parseInt(binStr, 2))).join('');
+};
+
+const encodeHeader = ({
+  version = 0,
+  cols = 8,
+  offsetX = 0,
+  offsetY = 0
+}) => {
+  // version is unused for now but in future we could add 1,2,&3 and decode strings differently without breaking existing urls
+  version = Math.min(3, version);
+  cols = Math.min(15, cols);
+  offsetX = Math.min(7, offsetX); // yes, this means you could have a 15 col island that starts way off to the right
+  offsetY = Math.min(7, offsetY); // and we can't encode all of that padding but I don't care. Pun intended: it's an edge case.
+
+  let headerBinary = '';
+  headerBinary = dec2bin(version, 2);
+  headerBinary += dec2bin(cols, 4);
+  headerBinary += dec2bin(offsetX, 3);
+  headerBinary += dec2bin(offsetY, 3);
+  return binary2string(headerBinary);
+};
+
+const HEADER_LENGTH = 2;
+
+const splitHeader = str => {
+  const { cols } = decodeHeader(str.slice(0, HEADER_LENGTH));
+  const islandChars = str.slice(HEADER_LENGTH).split('');
+  return {
+    cols,
+    islandChars
+  };
+};
+
 const island2String = (isle) => {
   let maxRowLen = 0;
-  const binStrs = flatten(trimEmptyPadding(isle).map(row => {
+  const bins = flatten(trimEmptyPadding(isle).map(row => {
     if (row.length > maxRowLen) {
       maxRowLen = row.length;
     }
-    const sixBitChunks = row.join('').match(/.{1,6}/g);
-    return (sixBitChunks || []).map(binStr => (binStr + '000000').slice(0, 6));
+    return row.join('').match(/.{0,6}/g).filter(b => b.length).map(bin => ('000000' + bin).slice(-6));
   }));
 
-  const chars = binStrs.map(bin => {
-    return ALPHABET.charAt(parseInt(bin, 2));
-  });
-
-  return ALPHABET.charAt(maxRowLen) + chars.join('');
+  return encodeHeader({ cols: maxRowLen }) + binary2string(bins.join(''));
 };
 
 const string2island = (str) => {
-  const rowLength = ALPHABET.indexOf(str.charAt(0));
-  const letters = str.slice(1).split('').map(char2bin).map(bin6 => bin6.split(''));
+  
+  const { cols, islandChars } = splitHeader(str);
+  const letters = islandChars.map(char => dec2bin(ALPHABET.indexOf(char))).map(bin6 => bin6.split(''));
   const island = [];
 
   for (let i = 0; i < letters.length; i++) {
     let row = [];
-    while (row.length < rowLength) {
+    while (row.length < cols) {
       // Add all the binary this letter encoded
       row = row.concat(letters[i]);
-      if (row.length >= rowLength) {
+      if (row.length >= cols) {
         // This is now too long; trim it down to length
-        row = row.slice(0, rowLength);
+        row = row.slice(0, cols);
         break;
       }
       // Get ready to look ahead to the next letter
@@ -75,7 +121,7 @@ const string2island = (str) => {
 }
 
   // Add a moat around the island if needed
-  let xMoat = 8 - rowLength;
+  let xMoat = 8 - cols;
   let yMoat = 8 - island.length;
   while (xMoat--) {
     const method = isEven(xMoat) ? 'push' : 'unshift';
@@ -104,6 +150,11 @@ console.log(str, IslandConverter.string2island(str).map(row=>row.join()));
 **/
 
 export {
+  string2binary as _string2binary,
+  binary2string as _binary2string,
+  encodeHeader as _encodeHeader,
+  decodeHeader as _decodeHeader,
+  dec2bin as _dec2bin,
   transpose as _transpose,
   flatten as _flatten,
   allZeroes as _allZeroes,
--- src/utils/islandParser/islandParser.test.js
+++ src/utils/islandParser/islandParser.test.js
@@ -1,12 +1,87 @@
 import {
   island2String,
   string2island,
-  _transpose,
-  _flatten,
-  _allZeroes
+  _string2binary as string2binary,
+  _binary2string as binary2string,
+  _dec2bin as dec2bin,
+  _transpose as transpose,
+  _flatten as flatten,
+  _allZeroes as allZeroes,
+  _encodeHeader as encodeHeader,
+  _decodeHeader as decodeHeader
 } from './islandParser';
 
+describe('header', () => {
+  test('encode cols', () => {
+    const header = encodeHeader({
+      version: 1,
+      cols: 2,
+      offsetX: 3,
+      offsetY: 4
+    });
+    expect(header).toBe('i6');
+    expect(header.length).toBe(2);
+    const decoded = decodeHeader(header);
+    expect(decoded.version).toBe(1);
+    expect(decoded.cols).toBe(2);
+    expect(decoded.offsetX).toBe(3);
+    expect(decoded.offsetY).toBe(4);
+  });
+  test('all zeroes', () => {
+    const header = encodeHeader({
+      version: 0,
+      cols: 0,
+      offsetX: 0,
+      offsetY: 0
+    });
+    expect(header.length).toBe(2);
+    const decoded = decodeHeader(header);
+    expect(decoded.version).toBe(0);
+    expect(decoded.cols).toBe(0);
+    expect(decoded.offsetX).toBe(0);
+    expect(decoded.offsetY).toBe(0);
+  });
+  test('all maxxed', () => {
+    const header = encodeHeader({
+      version: 3,
+      cols: 15,
+      offsetX: 7,
+      offsetY: 7
+    });
+    expect(header.length).toBe(2);
+    const decoded = decodeHeader(header);
+    expect(decoded.version).toBe(3);
+    expect(decoded.cols).toBe(15);
+    expect(decoded.offsetX).toBe(7);
+    expect(decoded.offsetY).toBe(7);
+  });
+});
+
 describe('Helpers', () => {
+
+  test('dec2bin', () => {
+    let bin = dec2bin(1);
+    expect(bin).toBe('000001');
+    bin = dec2bin(2);
+    expect(bin).toBe('000010');
+    bin = dec2bin(2,3);
+    expect(bin).toBe('010');
+    bin = dec2bin(7,3);
+    expect(bin).toBe('111');
+    bin = dec2bin(8,4);
+    expect(bin).toBe('1000');
+    bin = dec2bin(8,5);
+    expect(bin).toBe('01000');
+    bin = dec2bin(15,4);
+    expect(bin).toBe('1111');
+    bin = dec2bin(15,6);
+    expect(bin).toBe('001111');
+  });
+
+  test('string-binary', () => {
+    expect(string2binary('abc')).toBe('001010001011110110');
+    expect(binary2string(string2binary('abc'))).toBe('abc');
+  });
 
   test('transpose works', () => {
     const arr = [
@@ -22,7 +97,7 @@ describe('Helpers', () => {
       [3,6,9]
     ];
 
-    expect(_transpose(arr)).toEqual(transposed);
+    expect(transpose(arr)).toEqual(transposed);
     expect(arr).toEqual(originalArr);
   });
 
@@ -35,30 +110,33 @@ describe('Helpers', () => {
     ];
     const originalArr = [...arr];
 
-    expect(_transpose(_transpose(arr))).toEqual(originalArr);
+    expect(transpose(transpose(arr))).toEqual(originalArr);
   });
 
 
   test('flatten works', () => {
-    expect(_flatten([[1,2],[3,4]])).toEqual([1,2,3,4]);
+    expect(flatten([[1,2],[3,4]])).toEqual([1,2,3,4]);
   });
 
   test('allZeroes works', () => {
-    expect(_allZeroes([0])).toBe(true);
-    expect(_allZeroes([0,0])).toBe(true);
-    expect(_allZeroes([0,0,0])).toBe(true);
-    expect(_allZeroes([1])).toBe(false);
-    expect(_allZeroes([0,1])).toBe(false);
-    expect(_allZeroes([1,0])).toBe(false);
-    expect(_allZeroes([0,1,0])).toBe(false);
-    expect(_allZeroes([1,1])).toBe(false);
-    expect(_allZeroes([1,1,1])).toBe(false);
+    expect(allZeroes([0])).toBe(true);
+    expect(allZeroes([0,0])).toBe(true);
+    expect(allZeroes([0,0,0])).toBe(true);
+    expect(allZeroes([1])).toBe(false);
+    expect(allZeroes([0,1])).toBe(false);
+    expect(allZeroes([1,0])).toBe(false);
+    expect(allZeroes([0,1,0])).toBe(false);
+    expect(allZeroes([1,1])).toBe(false);
+    expect(allZeroes([1,1,1])).toBe(false);
   });
 });
 
 describe('string2island', () => {
   test('centers island', () => {
-    const string = '1-';
+    const string = island2String([
+      [[1]]
+    ]);
+
     const island = string2island(string);
     expect(island).toEqual([
       [0,0,0,0,0,0,0,0],
@@ -83,7 +161,7 @@ describe('island2string', () => {
       [0,0,0,0,0]
     ];
     const str = island2String(island);
-    expect(str).toBe('0');
+    expect(str).toBe('00');
 });
 
 test('handles completely full island', () => {
@@ -94,7 +172,7 @@ test('handles completely full island', () => {
     [1,1,1,1,1]
   ];
   const str = island2String(island);
-  expect(str).toBe('5wwww');
+  expect(str).toBe('50vvvv');
 });
 
   test('removes leading empty rows', () => {
@@ -106,7 +184,7 @@ test('handles completely full island', () => {
         [1,1,1,1,1]
       ];
       const str = island2String(island);
-      expect(str).toBe('5wcw');
+      expect(str).toBe('50vrv');
   });
 
   test('removes trailing empty rows', () => {
@@ -118,7 +196,7 @@ test('handles completely full island', () => {
       [0,0,0,0,0]
     ];
     const str = island2String(island);
-    expect(str).toBe('5wcw');
+    expect(str).toBe('50vrv');
   });
 
   test('removes leading and trailing empty rows', () => {
@@ -132,7 +210,7 @@ test('handles completely full island', () => {
         [0,0,0,0,0]
       ];
       const str = island2String(island);
-      expect(str).toBe('5wcw');
+      expect(str).toBe('50vrv');
   });
 
   test('does not remove empty rows inside an island', () => {
@@ -142,6 +220,6 @@ test('handles completely full island', () => {
       [1,1,1,1,1]
     ];
     const str = island2String(island);
-    expect(str).toBe('5w0w');
+    expect(str).toBe('50v0v');
   });
 });
